e1639bfac4cf63b25bfea35434456ffabef68624
Yvonne Yeh <code@yvonneyeh.com>
2021-03-31 14:22:24 -0700

    Solved #1108. Runtime: 28 ms, Memory Usage: 14.3 MB.

---
+++ 1108.py
@@ -0,0 +1,23 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+
+
+        # 1. look at every char of string
+        # 2. keep track of the new address
+        # 3. if the char is . then replace with [.]
+        # 4. if char is not . then add the char to new address
+        # 5. return new address
+
+        defangIP = ""
+
+        for char in address:
+            if char != ".":
+                defangIP += char
+            if char == ".":
+                defangIP += "[.]"
+        return defangIP
+
+
+# Runtime: 28 ms, faster than 78.88% of Python3 online submissions for Defanging an IP Address.
+# Memory Usage: 14.3 MB, less than 35.09% of Python3 online submissions for Defanging an IP Address.
+#
