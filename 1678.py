90f8fcc3f5fa4e3e57b767c93ef3557d81ffdaa9
Yvonne Yeh <code@yvonneyeh.com>
2021-04-03 16:38:48 -0700

    Solved #1678. Runtime: 24 ms, Memory Usage: 14.1 MB.

--- 1678.py
+++ 1678.py
@@ -9,3 +9,10 @@ class Solution:
 
 # Runtime: 24 ms, faster than 94.29% of Python3 online submissions for Goal Parser Interpretation.
 # Memory Usage: 14.3 MB, less than 40.58% of Python3 online submissions for Goal Parser Interpretation.
+
+    def interpret2(self, command: str) -> str:
+
+        return command.replace("()", "o").replace("(al)","al")
+
+# Runtime: 24 ms, faster than 94.29% of Python3 online submissions for Goal Parser Interpretation.
+# Memory Usage: 14.1 MB, less than 69.98% of Python3 online submissions for Goal Parser Interpretation.
