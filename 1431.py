07558a95c29e5785856d31684eae1a320d52c9d5
Yvonne Yeh <code@yvonneyeh.com>
2021-04-05 19:58:02 -0700

    Solved #1431. Runtime: 40 ms, Memory Usage: 14.3 MB.

--- 1431.py
+++ 1431.py
@@ -1,1 +1,15 @@
 # 1431. Kids With the Greatest Number of Candies
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxCandies = max(candies)
+        result = []
+        for i in range(len(candies)):
+            if candies[i]+extraCandies >= maxCandies:
+                result.append(True)
+            else:
+                result.append(False)
+        return result
+
+# Runtime: 40 ms, faster than 49.66% of Python3 online submissions for Kids With the Greatest Number of Candies.
+# Memory Usage: 14.3 MB, less than 22.83% of Python3 online submissions for Kids With the Greatest Number of Candies.
