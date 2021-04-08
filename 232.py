7ef7b2f4527f7d18161e219982bad169ba690952
Yvonne Yeh <code@yvonneyeh.com>
2021-04-08 16:23:24 -0700

    Solved #232. Runtime: 32 ms, Memory Usage: 14.3 MB.

--- 232.py
+++ 232.py
@@ -16,7 +16,7 @@ class MyQueue:
         """
         Push element x to the back of queue.
         """
-        
+
         while self.stack_back:
             self.stack_front.append(self.stack_back.pop())
 
@@ -26,7 +26,7 @@ class MyQueue:
         """
         Removes the element from in front of queue and returns that element.
         """
-        
+
         while self.stack_front:
             self.stack_back.append(self.stack_front.pop())
 
@@ -37,25 +37,61 @@ class MyQueue:
         """
         Get the front element.
         """
-        
+
         while self.stack_front:
             self.stack_back.append(self.stack_front.pop())
 
         return self.stack_back[-1]
 
-        
+
 
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
-    
+
         return not self.stack_front and not self.stack_back
 
-    
+
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+class Queue:
+    # initialize your data structure here.
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    # @param x, an integer
+    # @return nothing
+    def push(self, x):
+        self.stack1.append(x)
+
+    # @return nothing
+    def pop(self):
+        if len(self.stack2)!=0:
+            self.stack2.pop()
+        else:
+            while len(self.stack1)!=0:
+                self.stack2.append(self.stack1.pop())
+            self.stack2.pop()
+
+    # @return an integer
+    def peek(self):
+        if len(self.stack2)!=0:
+            return self.stack2[-1]
+        else:
+            while len(self.stack1)!=0:
+                self.stack2.append(self.stack1.pop())
+            return self.stack2[-1]
+
+    # @return an boolean
+    def empty(self):
+        if len(self.stack1)==0 and len(self.stack2)==0:
+            return True
+        else:
+            return False
