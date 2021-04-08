943799ecff40c2317a4cfcd784e5774d77617a74
Yvonne Yeh <code@yvonneyeh.com>
2021-04-08 16:20:17 -0700

    Solved #1704. Runtime: 36 ms, Memory Usage: 14.1 MB.

--- 1704.py
+++ 1704.py
@@ -33,3 +33,23 @@ class Solution:
 
 # Runtime: 36 ms, faster than 56.88% of Python3 online submissions for Determine if String Halves Are Alike.
 # Memory Usage: 14.4 MB, less than 37.61% of Python3 online submissions for Determine if String Halves Are Alike.
+
+    def halvesAreAlike2(self, s: str) -> bool:
+
+        vowels = set("aeiouAEIOU")
+
+        count1 = 0
+        count2 = 0
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            count1 += s[i] in vowels
+            count2 += s[j] in vowels
+            i += 1
+            j -= 1
+
+        return count1 == count2
+
+# Runtime: 36 ms, faster than 56.88% of Python3 online submissions for Determine if String Halves Are Alike.
+# Memory Usage: 14.1 MB, less than 88.26% of Python3 online submissions for Determine if String Halves Are Alike.
