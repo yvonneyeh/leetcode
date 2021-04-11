823f658616b9a7e012811ec6b794dd76d364458d
Yvonne Yeh <code@yvonneyeh.com>
2021-04-10 19:49:37 -0700

    Solved #1480. Runtime: 64 ms, Memory Usage: 14.4 MB.

---
+++ 1480.py
@@ -0,0 +1,22 @@
+# 1480. Running Sum of 1d Array
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+
+
+        # create output list
+        # loop through array, for range length of list
+        # keep sum counter and add nums to Sum
+        # append to new list
+
+
+        output = []
+        total = 0
+        for i in range(len(nums)):
+            total += nums[i]
+            output.append(total)
+
+        return output
+
+# Runtime: 64 ms, faster than 5.73% of Python3 online submissions for Running Sum of 1d Array.
+# Memory Usage: 14.4 MB, less than 72.25% of Python3 online submissions for Running Sum of 1d Array.
