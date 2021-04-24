e1158f0787f26ab73ac38b6b88f5146a6537b7cf
Yvonne Yeh <code@yvonneyeh.com>
2021-04-24 16:13:36 -0700

    #344. Reverse String

---
+++ 344.py
@@ -0,0 +1,12 @@
+# 344. Reverse String
+# Easy
+# Write a function that reverses a string. The input string is given as an array of characters s.
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+
+# Runtime: 192 ms, faster than 75.12% of Python3 online submissions for Reverse String.
+# Memory Usage: 18.7 MB, less than 52.05% of Python3 online submissions for Reverse String.
