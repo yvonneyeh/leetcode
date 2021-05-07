3e3e9f7dc32f0b31fca607d8aa6d779e71d0b238
Yvonne Yeh <code@yvonneyeh.com>
2021-05-07 13:41:53 -0700

    #74. Search a 2D Matrix - Binary Search

--- 74.py
+++ 74.py
@@ -1,4 +1,9 @@
-74. Search a 2D Matrix
+#74. Search a 2D Matrix
+# Medium
+# Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+# Integers in each row are sorted from left to right.
+# The first integer of each row is greater than the last integer of the previous row.
 
 class BinarySearch:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
