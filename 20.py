0b290557a93380ed297c34029bc0dbdfd47bd61e
Yvonne Yeh <code@yvonneyeh.com>
2021-06-15 18:09:42 -0700

    #20. Valid Parentheses

--- 20.py
+++ 20.py
@@ -31,3 +31,20 @@ class Solution:
                     return False
 
         return len(stack) == 0
+
+        # Runtime: 28 ms
+
+
+    def isValid_dict(self, s: str) -> bool:
+
+        stack = []
+        match = {')': '(', ']': '[', '}': '{'}
+        for paren in s:
+            if paren in match:
+                if not (stack and stack.pop() == match[paren]):
+                    return False
+            else:
+                stack.append(paren)
+        return not stack
+
+    # Runtime: 28 ms
