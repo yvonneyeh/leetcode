55fd370bc752617f539a56b74678abaa1021fc36
Yvonne Yeh <code@yvonneyeh.com>
2021-06-21 09:59:37 -0700

    #1528. Shuffle String

--- 1528.py
+++ 1528.py
@@ -6,3 +6,16 @@
 
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
+        res = [''] * len(s)
+        for i in range(len(s)):
+            res[indices[i]] = s[i]
+        return ''.join(res)
+
+    # Runtime: 48 ms, faster than 94.54% of Python3 online submissions for Shuffle String.
+    # Memory Usage: 14 MB, less than 91.99% of Python3 online submissions for Shuffle String.
+
+    def restoreString_sortedzip(self, s: str, indices: List[int]) -> str:
+        return "".join(c for _, c in sorted(zip(indices, s)))
+
+    # Runtime: 56 ms, faster than 59.20% of Python3 online submissions for Shuffle String.
+    # Memory Usage: 14.4 MB, less than 18.54% of Python3 online submissions for Shuffle String.
