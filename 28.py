bc30089d0c6b67faa766ba61a78a93bc1310ce42
Yvonne Yeh <code@yvonneyeh.com>
2021-06-27 14:01:33 -0700

    #28. Implement strStr()

--- 28.py
+++ 28.py
@@ -5,3 +5,16 @@ class Solution:
                 return i
 
         return -1
+
+    def strStr2(self, haystack, needle):
+        if needle == "":
+            return 0
+        for i in range(len(haystack)-len(needle)+1):
+            if (haystack[i:i + len(needle)] == needle):
+                    return i
+            for j in range(len(needle)):
+                if haystack[i+j] != needle[j]:
+                    break
+                if j == len(needle)-1:
+                    return i
+        return -1
