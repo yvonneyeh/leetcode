c7a7c4f2b0b6fe89e90f51a2bd624827e3891e8b
Yvonne Yeh <yvonne@hologram.io>
2022-07-23 11:32:52 -0700

    #1741. Find Total Time Spent by Each Employee

---
+++ 1741.py
@@ -0,0 +1,9 @@
+#1741. Find Total Time Spent by Each Employee
+
+# Write your MySQL query statement below
+SELECT
+    event_day AS day,
+    emp_id,
+    SUM(out_time - in_time) AS total_time
+FROM Employees
+GROUP BY day, emp_id; 
