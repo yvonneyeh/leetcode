d27afa5eb522844d99f271f6a18d45d6c0575f36
Yvonne Yeh <code@yvonneyeh.com>
2022-08-16 09:35:19 -0700

    2194. Cells in a Range on an Excel Sheet

---
+++ 2194.py
@@ -0,0 +1,9 @@
+class Solution:
+    def cellsInRange(self, s: str) -> List[str]:
+        a, b = map(str, s.split(":"))
+        result = []
+        r = range(ord(a[0]), ord(b[0])+1)
+        for i in r:
+            for j in range(int(a[-1]), int(b[-1])+1):
+                result.append(chr(i)+str(j))
+        return result
