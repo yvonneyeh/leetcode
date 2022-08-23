a7044e1cfc44ffcd6e43e4b1e71657c398a7ef08
Yvonne Yeh <code@yvonneyeh.com>
2022-08-23 16:20:45 -0700

    Stacks

--- 20.py
+++ 20.py
@@ -48,3 +48,47 @@ class Solution:
         return not stack
 
     # Runtime: 28 ms
+
+    def isValid(self, s: str) -> bool:
+        # create a stack to keep track of characters
+        # if it's an opening paren, push it to stack
+        # if it's a closing paren, pop it
+        # (())
+        # stack should be empty for valid paren
+
+        stack = []
+        paren_map = {
+            ')':'(',
+            '}':'{',
+            ']':'['
+        }
+
+        for c in s:
+            if c in paren_map:
+                if stack and stack[-1] == paren_map[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
+
+
+    class Solution:
+    def isValid(self, s: str) -> bool:
+
+        opened = ['(','{','[']
+        closed = [')','}',']']
+        stack = []
+
+        for c in s:
+            if c in opened:
+                stack.append(c)
+            else:
+                if stack and stack[-1] == opened[closed.index(c)]:
+                    stack.pop()
+                else:
+                    return False
+
+        return len(stack) == 0
