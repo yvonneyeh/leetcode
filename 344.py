0bfd5409de969822c5e026c43e40953f9b7c3708
Yvonne Yeh <code@yvonneyeh.com>
2022-08-26 13:18:13 -0700

    #344. Reverse String

--- 344.py
+++ 344.py
@@ -10,3 +10,22 @@ class Solution:
 
 # Runtime: 192 ms, faster than 75.12% of Python3 online submissions for Reverse String.
 # Memory Usage: 18.7 MB, less than 52.05% of Python3 online submissions for Reverse String.
+
+    def reverseString2(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+        return s
+
+        # O(n) time
+        # O(1) space
