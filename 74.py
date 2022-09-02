7dc266ecf6c2956f9846c553e13a19040fc5c04c
Yvonne Yeh <code@yvonneyeh.com>
2022-09-02 16:21:12 -0700

    #74. Search a 2D Matrix

--- 74.py
+++ 74.py
@@ -24,3 +24,34 @@ class BinarySearch:
             else:
                 r = mid
         return target == matrix[l//col][l % col]
+        
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        top, bottom = 0, ROWS - 1
+
+        while top <= bottom:
+            row = (top + bottom) // 2
+            if target > matrix[row][-1]: # last digit in row
+                top = row + 1 # move the top pointer down
+            elif target < matrix[row][0]: # first digit in row
+                bottom = row - 1
+            else:
+                break
+
+        if not (top <= bottom):  # invalid pointers
+            return False
+
+        l, r = 0, COLS - 1
+        row = (top + bottom) // 2
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target > matrix[row][mid]:
+                l = mid + 1
+            elif target < matrix[row][mid]:
+                r = mid - 1
+            else:  # found target
+                return True
+        return False
