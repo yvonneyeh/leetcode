03e4bf1b5cbacb39f3d9e07fc736342d82e2f370
Yvonne Yeh <code@yvonneyeh.com>
2022-09-02 16:20:33 -0700

    #200. Number of Islands

--- 200.py
+++ 200.py
@@ -36,31 +36,72 @@ Constraints:
     grid[i][j] is '0' or '1'.
 """
 
-def numIslands(self, grid: List[List[str]]) -> int:
+class BFS:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        directions = [[1, 0],[-1, 0],[0, 1],[0, -1]]
+                    # right, left, top, bottom
+
         if not grid or not grid[0]:
             return 0
 
-        islands = 0
-        visit = set()
-        rows, cols = len(grid), len(grid[0])
+        def bfs(r, c):
+            q = collections.deque()
+            visited.add((r,c))
+            q.append((r,c))
 
-        def dfs(r, c):
-            if (
-                r not in range(rows)
-                or c not in range(cols)
-                or grid[r][c] == "0"
-                or (r, c) in visit
-            ):
-                return
+            while q:
+                row, col = q.popleft()
 
-            visit.add((r, c))
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-            for dr, dc in directions:
-                dfs(r + dr, c + dc)
+                for dr, dc in directions:
+                    r = dr + row
+                    c = dc + col
+                    if (r in range(rows) and
+                        c in range(cols) and
+                       grid[r][c] == "1" and
+                       (r,c) not in visited):
+                        visited.add((r,c))
+                        q.append((r,c))
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visit:
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
                     islands += 1
-                    dfs(r, c)
+
         return islands
+
+
+
+class DFS:
+    def numIslands(self, grid: List[List[str]]) -> int:
+            if not grid or not grid[0]:
+                return 0
+
+            islands = 0
+            visit = set()
+            rows, cols = len(grid), len(grid[0])
+
+            def dfs(r, c):
+                if (
+                    r not in range(rows)
+                    or c not in range(cols)
+                    or grid[r][c] == "0"
+                    or (r, c) in visit
+                ):
+                    return
+
+                visit.add((r, c))
+                directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+                for dr, dc in directions:
+                    dfs(r + dr, c + dc)
+
+            for r in range(rows):
+                for c in range(cols):
+                    if grid[r][c] == "1" and (r, c) not in visit:
+                        islands += 1
+                        dfs(r, c)
+            return islands
