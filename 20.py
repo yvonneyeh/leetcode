0b2d78cff2255271fa4a74f81e2c8df87749b211
Yvonne Yeh <code@yvonneyeh.com>
2022-09-13 14:13:59 -0700

    #20. Valid Parentheses

--- 20.py
+++ 20.py
@@ -78,6 +78,16 @@ class Solution:
     class Solution:
     def isValid(self, s: str) -> bool:
 
+        # open and closing sets for constant lookup time
+        # init stack
+        # look at every char in s
+        # if char is in open, append to stack
+        # if not, check if there is item in stack and last item matches opening index
+            # pop item if valid
+            # else = not valid
+
+        # return true if len of stack is empty
+
         opened = ['(','{','[']
         closed = [')','}',']']
         stack = []
@@ -91,4 +101,22 @@ class Solution:
                 else:
                     return False
 
+        return len(stack) == 0
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+            
+        # if it's the left bracket then we append it to the stack
+        # else if it's the right bracket and the stack is empty(meaning no matching left bracket), or the left bracket doesn't match
+        # finally check if the stack still contains unmatched left bracket
+
+        parens = {'(':')', '{':'}','[':']'}
+        stack = []
+
+        for c in s:
+            if c in parens:
+                stack.append(c)
+            elif not stack or parens[stack.pop()] != c:
+                    return False
         return len(stack) == 0
