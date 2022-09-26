8981be2bfc59cc6cb7cd573a43fa4b0800cc391c
Yvonne Yeh <code@yvonneyeh.com>
2022-09-26 16:24:54 -0700

    # 143. Reorder List

---
+++ 143.py
@@ -0,0 +1,69 @@
+# 143. Reorder List
+"""
+Medium
+
+7230
+
+249
+
+Add to List
+
+Share
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+Example 2:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5 * 104].
+1 <= Node.val <= 1000
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return
+
+        # find the mid point
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse the second half in-place
+        pre, node = None, slow
+        while node:
+            pre, node.next, node = node, pre, node.next
+
+        # Merge in-place; Note : the last node of "first" and "second" are the same
+        first, second = head, pre
+        while second.next:
+            first.next, first = second, first.next
+            second.next, second = first, second.next
+        return 
