fab5f4fb68e43824d1386c3406fca61050bab0e3
Yvonne Yeh <code@yvonneyeh.com>
2022-11-01 08:36:14 -0700

    #146. LRU Cache

--- 146.py
+++ 146.py
@@ -43,3 +43,36 @@ Constraints:
 At most 2 * 105 calls will be made to get and put.
 
 """"
+
+# put :
+# if key is already in the cache, update the value, remove the key node then insert the key node after the head
+# if key is not in cache, and if the cache is not full, we insert the new key node after the head.
+# if the cache is full, we delete the node before the tail to make room for the new key node, then insert the new key node after the head.
+#
+# get:
+# return value of the key.
+# remove key node.
+# insert key node after the head.
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.d = {}
+        self.size = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.d: return -1
+        self.d[key] = self.d.pop(key)
+        return self.d[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.d: self.d.pop(key)
+        self.d[key] = value
+        if len(self.d) > self.size:
+            self.d.pop(next(iter(self.d)))
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
