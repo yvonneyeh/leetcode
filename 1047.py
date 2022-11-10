651b400b29177f48c1b3ea94b5ed98dab12e0887
Yvonne Yeh <code@yvonneyeh.com>
2022-11-10 08:12:21 -0800

    #1047. Remove All Adjacent Duplicates In String

--- 1047.py
+++ 1047.py
@@ -27,3 +27,16 @@ Constraints:
 1 <= s.length <= 105
 s consists of lowercase English letters.
 """
+
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+
+        for char in s:
+            if stack and char == stack[-1]:
+                stack.pop()
+
+            else:
+                stack.append(char)
+
+        return ''.join(stack)
