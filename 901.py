4c7b6c810d8524fc65dcf404a7b3e542f3c88adb
Yvonne Yeh <code@yvonneyeh.com>
2022-11-10 08:51:41 -0800

    #901. Online Stock Span

--- 901.py
+++ 901.py
@@ -39,11 +39,16 @@ At most 104 calls will be made to next.
 """
 
 class StockSpanner:
-
     def __init__(self):
-
+        self.stack = []  # pair: (price, span)
 
     def next(self, price: int) -> int:
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack[-1][1]
+            self.stack.pop()
+        self.stack.append((price, span))
+        return span
 
 
 
