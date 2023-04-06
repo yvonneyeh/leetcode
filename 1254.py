45ce541b6be5ca1023e4b233b5dedd7a46f06a26
Yvonne Yeh <yvonne.yeh@cultureamp.com>
2023-04-06 12:26:01 -0700

    #1254. Number of Closed Islands

--- 1254.py
+++ 1254.py
@@ -34,3 +34,33 @@ Constraints:
 1 <= grid.length, grid[0].length <= 100
 0 <= grid[i][j] <=1
 """
+
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+
+        if not grid:
+            return 0
+
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r == ROWS or c == COLS):
+                return 0
+            if grid[r][c] == 1 or (r, c) in visit:
+                return 1
+
+            visit.add((r,c))
+            return min(dfs(r+1, c),
+                    dfs(r, c+1),
+                    dfs(r-1, c),
+                    dfs(r, c-1))
+
+            res = 0
+
+            for r in range(ROWS):
+                for c in range(COLS):
+                    if not grid[r][c] and (r,c) not in visit:
+                        res += dfs(r,c)
+
+            return res
